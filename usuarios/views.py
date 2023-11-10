68115bd7f7add0a4b6ec77d55f25cfb2153af0a6
EliasFK <eliasfrank58@gmail.com>
2023-11-10 18:56:18 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.get(username=username)
+        if user:
+            return HttpResponse('já existe um usuario com esse username!')
+
+        return HttpResponse(username)
+
+
+def login(request):
+    return render(request, 'login.html')
